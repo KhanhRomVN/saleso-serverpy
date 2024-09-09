f5ff83f688954be572d614dc391e9c76603975cc
KhanhRomVN <khanhromvn@gmail.com>
2024-09-09 15:43:53 +0700

    update

--- pyserver/settings.py
+++ pyserver/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'pyserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates/'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
